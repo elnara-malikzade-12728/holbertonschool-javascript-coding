5e015cd4e49ca49232a8e247b826a5535f02d16c
Elnara Malikzade <12728@holbertonstudents.com>
2026-05-02 11:06:47 +0000

    Add task12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeeList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
