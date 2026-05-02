3aa7d30268b7296365403906282bdbb136d259cb
Elnara Malikzade <12728@holbertonstudents.com>
2026-05-02 09:36:43 +0000

    Add task10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const item of array) {
     const idx = array.indexOf(item);
-    array[idx] = appendString + value;
+    array[idx] = appendString + item;
   }
 
   return array;
