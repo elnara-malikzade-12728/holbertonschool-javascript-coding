b5c5570dd82ac2bb153d187fe6f280dec8ad4c90
root <12728@holbertonstudents.com>
2026-05-03 09:50:19 +0000

    Add task0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+export default function getListStudents() {
+    const id = 0;
+    const firstName = "";
+    const location = "";
+    return [
+	{id: 1, firstName: "Guillaume", location: "San Francisco"},
+	{id: 2, firstName: "James", location: "Columbia"},
+	{id: 5, firstName: "Serena", location: "San Francisco"}
+    ];
+}
