53490269dffd43990864b5be8c6e8aed66b38eec
Elnara Malikzade <12728@holbertonstudents.com>
2026-05-04 11:15:57 +0000

    Modify the file

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,8 +1,11 @@
 import {uploadPhoto, createUser} from './utils.js';
+
 export default function handleProfileSignup() {
-    const promise1 = uploadPhoto();
-    const promise2 = createUser();
-    return Promise.all([promise1, promise2])
-        .then((values) => console.log(values))
-        .catch((error) => console.log('Signup system offline'));
+    return Promise.all([uploadPhoto(), createUser()])
+        .then((values) => {
+	    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+	})
+        .catch((error) => {
+	    console.log('Signup system offline');
+	});
 }
