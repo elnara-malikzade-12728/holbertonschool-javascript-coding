562fb6a42911f2d17e1f3f4f2d027c09cc9c429e
Elnara Malikzade <12728@holbertonstudents.com>
2026-05-04 10:34:41 +0000

    Modify task2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,7 @@
 export default function handleResponseFromAPI(promise) {
     return promise
-	.then(() => {
-	    console.log({status: 200, body: 'success'});
-	})
-        .catch(() => {
-	    console.log(new Error());
-	})
+	.then(() => ({status: 200, body: 'success'}))
+        .catch(() => new Error())
         .finally(() => {
 	    console.log('Got a response from the API');
 	})
