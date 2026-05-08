40c2aed2b8ae0e3675d99e5cea1f726a5b1319b5
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-05-08 21:41:36 +0400

    Add task2

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,6 @@
+function welcomeMessage(fullName) {
+    alert('Welcome ' + fullName);
+}
+const guillame = welcomeMessage('Guillaume');
+const alex = welcomeMessage('Alex');
+const fred = welcomeMessage('Fred');
