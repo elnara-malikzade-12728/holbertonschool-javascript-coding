9114b8da26c8059b59c5c759e37cab0ed099274e
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-05-09 14:54:33 +0400

    Add task8

---
+++ Javascript_advanced/8-payments.js
@@ -0,0 +1,13 @@
+function proccessPayment(amount) {
+    console.log(`Collecting payment of `+ amount);
+}
+function processOrder(orderId, amount){
+    console.log(orderId + ` is being processed`);
+    proccessPayment(amount);
+    console.log(orderId + ` has been fully processed`);
+}
+console.log(`Processing orders`);
+processOrder(12321, 10.99);
+processOrder(12322, 12.99);
+processOrder(12323, 15.0);
+console.log(`All the orders have been processed`);
