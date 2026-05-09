fdae55b95773cdf8fc83552ce839f17547953087
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-05-09 10:14:24 +0400

    Add task5

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -13,7 +13,7 @@ function main() {
     const screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
 
     const paragraph = document.createElement('p');
-    paragraph.textContent= 'Welcome holberton!';
+    paragraph.textContent= 'Welcome Holberton!';
     document.body.appendChild(paragraph);
 
     const spookyButton = document.createElement('button');
@@ -22,12 +22,12 @@ function main() {
     document.body.appendChild(spookyButton);
 
     const darkModeButton = document.createElement('button');
-    darkModeButton.textContent = 'Dark Mode';
+    darkModeButton.textContent = 'Dark mode';
     darkModeButton.addEventListener('click', darkMode);
     document.body.appendChild(darkModeButton);
 
     const screamModeButton = document.createElement('button');
-    screamModeButton.textContent = 'Scream Mode';
+    screamModeButton.textContent = 'Scream mode';
     screamModeButton.addEventListener('click', screamMode);
     document.body.appendChild(screamModeButton);    
 
