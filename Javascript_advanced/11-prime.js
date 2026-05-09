8d3101f7e81b94a57a735099066bf9fcdcd725af
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-05-09 19:03:11 +0400

    Add task11

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,27 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) count++;
+    }
+    return count;
+}
+
+const start = performance.now();
+setTimeout(() => {
+    for (let i = 1; i <= 100; i++){
+    countPrimeNumbers();
+    }
+});
+
+const end = performance.now();
+
+const timeUsed = end - start;
+
+console.log(`Execution time of calculating prime numbers 100 times was ${timeUsed} milliseconds.`);
