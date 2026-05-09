3ccd519e42009d916a7d4d07834af506c6a63904
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-05-09 19:18:52 +0400

    Add task12

--- Javascript_advanced/12-room_area.js
+++ Javascript_advanced/12-room_area.js
@@ -1,9 +1,10 @@
-class RoomDimensions {
-    width = 50;
-    length = 100;
-    getArea(width, length){
+ const roomDimensions = {
+    width: 50,
+    length:100,
+
+    getArea(){
         return this.width * this.length;
     }
-}   
-const boundGetArea = new RoomDimensions();
-console.log(boundGetArea.getArea());
+};   
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+console.log(boundGetArea());
