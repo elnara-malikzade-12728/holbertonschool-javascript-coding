84ab01e4e116dc753f70c620bf17f4f8c4a82c14
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-05-09 11:14:45 +0400

    Add task6

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,37 @@
+class StudentHogwarts {
+    #privateScore = 0;
+    name = null;
+    #changeScoreBy(points) {
+        this.#privateScore += points;
+        return this.#privateScore;
+    }
+    setName(newName){
+        this.name = newName;
+        return newName;
+    }
+    rewardStudent(){
+        this.#changeScoreBy(1); 
+    }
+    penalizeStudent(){
+        this.#changeScoreBy(-1);
+    }
+    getScore(){
+        return `${this.name}: ${this.#privateScore}`;
+    }
+
+}
+const harry = new StudentHogwarts();
+harry.setName('Harry');
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+const draco = new StudentHogwarts();
+draco.setName('Draco');
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
