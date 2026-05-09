d3ccb66ddd79b99b43df3362570b2c7c948fdc89
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-05-09 15:31:39 +0400

    Add task9

--- Javascript_advanced/9-prime.js
+++ Javascript_advanced/9-prime.js
@@ -1,11 +1,22 @@
+const { performance } = require('perf_hooks');
 const start = performance.now();
 function countPrimeNumbers() {
     let count = onabort;
-    for (let i = 2; i <= 100; i++){
-        if (i / 1 == i || i / i == 1){
-            count ++;
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            count++;
         }
     }
+
     return count;
 }
 const end = performance.now();
