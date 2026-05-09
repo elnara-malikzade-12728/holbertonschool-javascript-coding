571e386f85635e9e73906c540874ccad9940d263
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-05-09 19:34:53 +0400

    Add task13

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,16 @@
+const user = {
+    hobby: 'Calligraphy',
+    favoriteSport: 'Hockey',
+    astrologicalSign: 'Aries',
+    firstName: 'Guillaume',
+    lastName: 'Johns',
+    location: 'Netherlands',
+    occupation: 'Engineer',
+
+    logWelcomeUser(welcomeString){
+        console.log(welcomeString + ' ' + user.firstName + `. Your occupation is: ` + user.occupation)
+    }
+
+}
+const bindLogWelcomeUser = user.logWelcomeUser.bind(user);
+console.log(bindLogWelcomeUser('Welcome'));
